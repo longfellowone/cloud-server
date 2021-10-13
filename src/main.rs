a8807c405ad0256b09e6ef06356d4b74ce3cf42b
Matt Wright <mattwright3195@gmail.com>
2021-10-13 07:37:38 -0700

    change ip

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ async fn main() -> std::io::Result<()> {
             .route("/postgres", web::get().to(postgres))
             .route("/search", web::get().to(search))
     })
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
